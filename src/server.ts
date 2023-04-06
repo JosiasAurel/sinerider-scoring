886271e30b1d0f7d14a34816a7d36820cf3392ed
graham <graham@hackclub.com>
2023-04-06 05:08:44 -0400

    Process one scoring request at a time & clean up code a bit (#6)
    * process one scoring request at a time
    
    * More touchups/cleanup

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { getScoresByLevel, saveSolution, getAllScores, saveLevel, getUnplayedLevel } from "./airtable.js";
-import { playLevel, getCharCount, generateLevel } from "./main.js";
+import { playLevel, generateLevel, ScoringResult } from "./main.js";
 import { nanoid } from "nanoid";
 import { uploadVideo } from "./video.js";
 import { accessSync, constants, rmSync, watchFile } from "fs";
@@ -10,12 +10,13 @@ import fs from 'fs'
 import path from 'path'
 import os from 'os'
 import {v4 as uuidv4} from 'uuid';
+import PQueue from 'p-queue';
+import { TimeoutError } from "puppeteer";
+import { SINERIDER_URL_PREFIX } from "./config.js";
 
 const app = express();
 
-// setup json
 app.use(express.json());
-// should fix cors issues
 app.use(cors());
 
 const port = process.env.PORT ?? 3000;
@@ -38,42 +39,60 @@ app.get("/all", (req, res) => {
     .catch((err) => res.json({ success: false, reason: err }));
 });
 
-// will return either a { success: true, id: <ID_OF_RECORD> } if successfully saved
-// or { success: false } if failed
 app.post("/score", async (req, res) => {
-  console.log("Starting scoring...")
-  // level is a url to the body to the game from the user
-  const { level } = req.body;
+  try {
+    const level = req.body.level;
 
-  let videoName: string = makeVideoName();
-  function makeVideoName(): string {
-    let name = `${nanoid(8)}.webm`;
-    if (name.startsWith("-")) return makeVideoName();
-    return name;
+    if (!level.startsWith(SINERIDER_URL_PREFIX)) {
+      res.status(400).json({message:`Invalid level URL (must start with ${SINERIDER_URL_PREFIX})`})
+      return
+    }
+
+    const result = await addScoringJob(level);
+    res.status(200).json(result);  
+  } catch (e) {
+    if (e instanceof TimeoutError) {
+      res.status(408).json({message:"Failed scoring due to timeout"})
+    } else {
+      res.status(500).json({message:"Internal server error"})
+    }
   }
+});
 
-  let solution: Solution;
-  console.log("Starting playLevel...")
-  
-  const tempDir = os.tmpdir()
-  const uuid = uuidv4()
-  const fullDir = tempDir.endsWith("/") ? tempDir + uuid : tempDir + "/" + uuid
-  console.log(`Making directory: ${fullDir}`)
-  fs.mkdtemp(fullDir, (err, folder) => {
-    if (err) throw err;
-    console.log(`Actual temp directory: ${folder}`)    
-
-    playLevel(level, videoName, folder).then((result) => {
-      res.json(result);
-      //finishWork(videoName, res, solution);    
+// Process scoring jobs one at a time.
+const queue = new PQueue({ concurrency: 1 });
+async function addScoringJob(level: string) {
+  return await new Promise<ScoringResult>(async (resolve, reject) => {
+    return await queue.add(async () => {
+      try {
+        resolve(await score(level));
+      } catch (e) {
+        reject(e);
+      }
     });
-    
-    // Clean up afterwards
-    console.log("Attempting to clean up folder: " + folder)
-    fs.rmSync(folder, { recursive: true, force: true });
+  });  
+}
+
+async function score(level:string) {
+  let videoName: string = makeVideoName();
+
+  console.log("Starting playLevel...")
+  const folder = await new Promise<string>((resolve, reject) => {
+    fs.mkdtemp(os.tmpdir().endsWith("/") ? os.tmpdir() + uuidv4() : os.tmpdir() + "/" + uuidv4(), async (err, f) => {
+      if (err) reject(err.message)
+      console.log(`Temp directory: ${f}`)
+      resolve(f)    
+    });  
   });
 
-});
+  try {
+    return await playLevel(level, videoName, folder)
+  } finally {
+    // Clean up afterwards
+    console.log("Cleaning up temp directory: " + folder)
+    fs.rmSync(folder, { recursive: true, force: true });
+  }
+}
 
 app.get("/daily", (_, res) => {
   getUnplayedLevel()
@@ -92,51 +111,8 @@ app.listen(port, () =>
   console.log(`Doing some black magic on port ${port}...`)
 );
 
-
-function finishWork(res: Response<any, Record<string, any>, number>, solution: Solution) {
-  const videoName = "PLEASE_FIX_ME";
-  uploadVideo(videoName)
-    .then((result) => (solution.gameplay = result?.uri ?? ""))
-    .then(() => {
-      saveSolution(solution)
-        .then((data: any) => // string ? { id: string }
-          res.json({ success: true, id: data.id, ...solution })
-        )
-        .then(() => {
-          // rmSync(videoName); // remove video after upload
-        })
-        .catch((err) => res.json({ success: false, reason: err }));
-    });
-  const fileExistCheck = setInterval(() => {
-    try {
-      accessSync(videoName, constants.F_OK);
-      watchFile(videoName, { bigint: false, persistent: true, interval: 1000 }, (curr, prev) => {
-        const diffSeconds = (curr.mtimeMs - prev.mtimeMs) / 1000;
-        if (diffSeconds >= 5) {
-
-        }
-      });
-
-      clearInterval(fileExistCheck);
-    } catch { }
-  }, 1000);
-
-  /*
-  setTimeout(() => {
-    try {
-      accessSync(videoName, constants.F_OK);
-      uploadVideo(videoName)
-        .then((result) => (solution.gameplay = result.uri))
-        .then(() => {
-          saveSolution(solution)
-            .then((data) =>
-              res.json({ success: true, id: data.id, ...solution })
-            )
-            .catch((err) => res.json({ success: false, reason: err }));
-        });
-
-      // clearInterval(fileExistCheck);
-    } catch { }
-  }, 10000);
-  */
+function makeVideoName(): string {
+  let name = `${nanoid(8)}.webm`;
+  if (name.startsWith("-")) return makeVideoName();
+  return name;
 }
