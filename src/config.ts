c9e544b32e8261c41cbab504bc4bf43f072d5db5
graham <graham@hackclub.com>
2023-05-06 18:22:32 -0400

    Allow for production sinerider URLs (#18)

--- src/config.ts
+++ src/config.ts
@@ -7,5 +7,6 @@ export const CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
 export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
 export const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
 export const SINERIDER_URL_PREFIX = process.env.SINERIDER_URL_PREFIX;
+export const SINERIDER_DEV_URL_PREFIX = process.env.SINERIDER_DEV_URL_PREFIX;
 export const SINERIDER_SCORING_PUBLIC_SSL_CERT = Buffer.from(process.env.SINERIDER_SCORING_PUBLIC_SSL_CERT as string, 'base64').toString('ascii');
 export const SINERIDER_SCORING_PRIVATE_SSL_KEY = Buffer.from(process.env.SINERIDER_SCORING_PRIVATE_SSL_KEY as string, 'base64').toString('ascii');
